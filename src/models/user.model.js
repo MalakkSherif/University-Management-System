a96a8456a9bded7482125b29bdd8346a5e826531
Nada Shaban <nadashaban971@gmail.com>
2025-09-06 14:18:10 +0300

    Add user model

---
+++ src/models/user.model.js
@@ -0,0 +1,36 @@
+
+const mongoose = require('mongoose');
+const  bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true , 'email is required'],
+        unique: true
+    },
+    password: {
+      type: String,
+      required: [true, 'password is required'],
+      minlength: [6 , 'password must be at least 6 characters']
+    },
+    role: { 
+        type: String, 
+        enum: ['admin', 'teacher', 'student'], 
+        required: [true, 'role is required'] 
+    }
+});
+
+userSchema.pre('save' , async function (next){
+    if (!this.isModified("password")) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.comparePass = async function  (pass) {
+    return await bcrypt.compare(pass , this.password);
+};
+
+
+module.exports = mongoose.model('User' , userSchema);
