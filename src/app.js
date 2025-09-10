209db8525e6279ffa111301146567f456c1635dc
Nada Shaban <nadashaban971@gmail.com>
2025-09-11 02:39:13 +0300

    edit connection

--- src/app.js
+++ src/app.js
@@ -2,16 +2,34 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 const authRouter = require('./routes/auth.routes.js');
+const connect = require('./config/dbConnection.js');
+
 
 dotenv.config();
+
 const app = express();
 
 
 app.use(express.json());
 
+app.use('/api/auth', authRouter);
 
 
-app.use('/api/ideas', ideaRouter);
 
+const port = process.env.PORT || 8000;
 
-export default app;
+const start = async () => {
+  try {
+    await connect();
+    app.listen(port, () => {
+      console.log(`DB connected & Server running on port ${port}`);
+    });
+  }
+  
+  catch (err) {
+    console.log('failed to connect', err);
+    process.exit(1);
+  }
+};
+
+start();
