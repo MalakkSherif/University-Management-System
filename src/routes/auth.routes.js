0ea7d5d6112ff5e6a4b798cbc7dfb159ab2c0260
Nada Shaban <nadashaban971@gmail.com>
2025-09-12 04:17:31 +0300

    add register route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { login, changePassword } = require("../controllers/user.controller.js");
+const {register , login, changePassword } = require("../controllers/user.controller.js");
 const { authMiddleware, restrictTo } = require("../middlewares/auth.middleware.js");
 const {loginValidation , changePasswordValidation, validate} = require('../middlewares/auth.validation.js')
 
 const authRouter = express.Router();
 
 
-// authRouter.post("/register", authMiddleware, restrictTo("admin"), register);
+authRouter.post("/register", authMiddleware, restrictTo("admin"), register);
 
 authRouter.post("/login", loginValidation, validate, login);
 authRouter.post("/change-password", authMiddleware, changePasswordValidation, validate, changePassword);
